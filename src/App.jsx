1b8cc5aa75bda2346f9ea69a353a48474fc49a7e
so_hell <armanyppph@gmail.com>
2023-04-07 12:34:29 +0530

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,13 @@ function App() {
     <>
       <h1>CUSTOM HOOKS</h1>
       <div className="text">
-      <p>Type something in the value and this will be automatically save in the local storage.</p>
+      <p>Type something in the input and the value will be automatically save in the local storage.</p>
       <p>We are doing this with the help of custom hook 'useLocalStorage'</p>
       </div>
       <input
         type={"text"}
         value={name}
+        placeholder="Type something"
         onChange={(e) => setName(e.target.value)}
       />
     </>
